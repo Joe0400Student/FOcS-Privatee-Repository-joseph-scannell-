683e3cef813773780e9545283c35465970cacfbf
joe0400Student <joseph_Scannell@student.uml.edu>
2020-10-24 21:15:01 -0400

    redid DFA and added assignment 12

--- p2/DFA.py
+++ p2/DFA.py
@@ -1,29 +1,55 @@
+from typing import Generic, List
+
+T = TypeVar('T')
 class DFA:
     
-    def __init__(self, accepting, name=""):
-        self.accepted = accepting
-        self.name = name
+    def __init__(self, Q: list, alpha: list, TF: List[List[int]], SS: int:, AS:List[bool]):
+        self.Q, self.alpha, self.TF, self.SS, self.AS = Q, alpha, TF, SS, AS
     
-    def next(self, item):
-        return self.next_table[item]
-
-    def add_next_table(self, table):
-        self.next_table = table
+    def __repr__(self, string_to_iterate):
+        state = self.SS
+        s = [str(Q[state])]
+        for c in string_to_iterate:
+            state = TF[state][alpha.index(c)]
+            s.append(str(Q[state]))
+        return '->'.join(s)
     
-    def accepting(self):
-        return self.accepted
-
-    def __repr__(self):
-        return self.name
+    def iterate_DFA(self,string_to_iterate):
+        state = self.SS
+        iterate = [alpha.index(c) for c in string_to_iterate]
+        for i in iterate:
+            state = TF[state][i]
+        return AS[state]
     
-    def geT_configurations(DFA_node, choices):
-        positions = []
-        for c in choices:
-            positions.append(str(DFA_node))
-            DFA_node = DFA_node.next(c)
-        
-        return '->'.join(positions + [str(DFA_node)])
-
     def __invert__(self):
-        self.accepted = not self.accepted
-        return self
+        AS = [not v for v in self.AS]
+        return DFA(self.Q,self.alpha,self.TF,self.SS,AS)
+    
+
+## DFA worker was orignially gonna be BFS, but hard so no...
+class DFAWorker:
+    
+    def __init__(DFA):
+        self.DFA = DFA
+        self.state = DFA.SS
+        self.previous = []
+        self.alpha = DFA.alpha
+    
+    def __init__(DFA, state, previous=[], characters=[]):
+        self.DFA = DFA
+        self.state = state
+        self.previous = previous
+        self.alpha = DFA.alpha
+        
+    def DFS(self):
+        if(self.DFA.AS[self.state]):
+            return ''.join(self.characters)
+        previous = self.previous + [self.state]
+        for c in self.alpha:
+            state = self.DFA.TF[self.state][self.alpha.index(c)]
+            if(state not in previous):
+                ret_val = DFAWorker(self.DFA,state,previous,self.characters + [c]).DFS()
+                if(ret_val != False):
+                    return ret_val
+        return False
+
