54b9cb31a7d8764bf5339d73b5f1d82cd8ea0bf8
joe0400Student <joseph_Scannell@student.uml.edu>
2020-11-16 18:52:06 -0500

    oracle added

--- p2/NFA.py
+++ p2/NFA.py
@@ -11,7 +11,19 @@ class NFA:
     def __init__(self,DFA):
         self.states = DFA.Q
         self.alpha = DFA.alpha
-        self.transition = {k:[v] for k,v in DFA.TF.items()}
+        self.transition = {k:{v_k:[v_v] for v_k,v_v in v.items()} for k,v in DFA.TF.items()}
         self.start_s = DFA.SS
         self.accepting = DFA.AS
+    
+    
+    def __pow__(self, data):
+        string, trace, boolean = data
+        state = self.start_s
+        if(state == trace[0]):
+        for next_s,c in zip(trace,string):
+            if(next_s not in self.transition[state][c]):
+                return False
+            state = next_s
+        return self.accepting[state] == boolean
+    
     
