579a2ca2b3ed594779d8a721fc81266f968d2a5d
joe0400Student <joseph_Scannell@student.uml.edu>
2020-11-18 02:42:28 -0500

    added forking and edited trace tree, added test suite library

--- p2/NFA.py
+++ p2/NFA.py
@@ -1,3 +1,22 @@
+import math
+def decorate(s,final=False):
+    a = "".join(["=" for i in range(50)])
+    spaces = " ".join(['' for i in range(100)])
+    def wrap(func):
+        def _():
+            print(f"{a}{s}{a}"[math.floor(len(s)/2):-math.ceil(len(s)/2)-2]+"\\\\")
+            try:
+                func()
+                print(f"||{s} PASSED{''.join([' ' for i in range(100)])}"[:98]+"||")
+            except AssertionError as e:
+                print(f"||AssertionError: {e}{spaces}"[:98]+"||")
+                if(not final):
+                    assert False, f"{s}: {e}"
+            finally:
+                print("\\\\" + "".join(["_" for i in range(100)])[2:])
+        return _
+    return wrap
+
 class NFA:
 
     def __init__(self, States=None, Alphabet=None, Transition_function=None, Start_State=None, Accepting_States=None,DFA=None):
@@ -13,16 +32,34 @@ class NFA:
     def __pow__(self, data):
         string, trace, boolean = data
         state = self.start_s
-        if(state == trace[0]):
-            for next_s,c in zip(trace,string):
-                if(next_s not in self.transition[state][c]):
-                    return False
-                state = next_s
+        if(state != trace[0]):
+            return False
+        for next_s,c in zip(trace[1:],string):
+            if(next_s not in self.transition[state][c]):
+                return False
+            state = next_s
         return self.accepting[state] == boolean
+    
+    def fork(self):
+        return {self.start_s:DFS(self,self.start_s)}
+def DFS(nfa,state):
+    current_dict = {}
+    for key in nfa.transition[state]:
+        temp_node = {}
+        for states in nfa.transition[state][key]:
+            if((val := DFS(nfa,states)) != None):
+                temp_node[states] = val
+        if(len(temp_node) != 0):
+            current_dict[key] = temp_node
+    
+    if(len(current_dict) != 0):
+        return current_dict
+    return None
+
 
 FizzBuzz = NFA(
     States=["initial","nmod3=1","nmod5=1","nmod3=2","nmod5=2","nmod3=0","nmod5=3","nmod5=4","nmod5=0"],
-    Alphabet="0",
+    Alphabet=["0"],
     Transition_function={
         "initial":{"":[],"0":["nmod3=1","nmod5=1"]},
         "nmod3=1":{"":[],"0":["nmod3=2"]},
@@ -35,32 +72,29 @@ FizzBuzz = NFA(
         "nmod5=0":{"":[],"0":["nmod5=1"]}
     },
     Start_State="initial",
-    Accepting_States=["nmod5=0","nmod3=0","initial"]
+    Accepting_States={"nmod5=0":True,"nmod3=0":True,"initial":True,"nmod3=1":False,"nmod5=1":False,
+                      "nmod3=2":False,"nmod5=2":False,"nmod5=3":False,"nmod5=4":False}
 )
+"""
+    0
+    accepting
+    nmod3=1,nmod5=1
+    00000
+    accepting
+    nmod3=1,nmod5=1
+    nmod3=2,nmod5=2
+    nmod3=0,nmod5=3
+    nmod3=1,nmod5=4
+    nmod3=2,nmod5=0
+    fail   ,accepting
+"""
+@decorate("FizzBuzzTest")
+def FizzBuzzTest():
+    assert FizzBuzz ** ("000",["initial","nmod3=1","nmod3=2","nmod3=0"],True), "Doesnt work"
+    assert not FizzBuzz ** ("000",["initial","nmod3=1","nmod5=0"],False),"Doesnt work"
 
-FizzBuzz ** (a,b,c,d)
-class Tree:
-    
-    def __init__(self, val=None ,sub_elements = []):
-        self.sub_elements, self.val = sub_elements, val
-    
-    def __repr__(self):
-        return f"{self.val}:branch({self.sub_elements})"
-    
-    def __getitem__(self,index):
-        return self.sub_elements[index]
-    
-    def insert(self,item):
-        self.sub_elements.append(item)
-    
-    def __setitem__(self,index,value):
-        self.sub_elements[index] = value
-    
-class Trace:
-    
-    def __init__(self, state, accepting):
-        self.state, self.accepting = state, accepting
-    
-    def __repr__(self):
-        return f"{self.state}|{self.accepting}"
+@decorate("TestSuite",True)
+def test_suite():
+    FizzBuzzTest()
 
+test_suite()
